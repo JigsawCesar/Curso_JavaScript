e1dbde8cd6949fa6f05c95e1e4ecbed7baaaee3a
JigsawCesar <felipeportela_@hotmail.com>
2026-03-18 11:58:09 -0300

    subindo atualizações 18/03

---
+++ Turma_manhã/Arrays/Exercicios_Array_Objetos2/diario_leitura.js
@@ -0,0 +1,31 @@
+const prompt = require(`prompt-sync`)();
+
+let aluno = {
+    nome: Felipe,
+    pontosLeitura: 10,
+    historicoDias: [
+        registro1 = {
+            data: `01-06`,
+            paginasLidas: 35 
+    }
+        , registro2 = {
+            data: `02-06`,
+            paginasLidas: 20
+    }]
+};
+
+let registro3 = {
+    data: prompt(`Digite a data de hoje: `),
+    paginasLidas: parseInt(prompt(`Digite a quantidade de páginas lidas: `))
+};
+
+aluno.historicoDias.push(registro3);
+
+if (aluno.historicoDias[2])) {
+    
+} else {
+    
+}
+
+
+
--- Turma_manhã/Arrays/Exercicios_Array_Objetos2/gestao_eventos.js
+++ Turma_manhã/Arrays/Exercicios_Array_Objetos2/gestao_eventos.js
@@ -2,17 +2,35 @@ const prompt = require(`prompt-sync`)()
 
 let grupo = {
     nome: `Lions`,
-    orcamentoTotal: 500,
+    orcamentoTotal: parseFloat(500),
     openBarFechado: false,
     consumoExtras: []
 };
 
-
 let bebidas = {
-    nome: (prompt(`Digite o nome da bebida importada: `)),
-    valorTotal: (prompt(`Digite o valor da bebida importada: `))
-};
+    nome1: (prompt(`Digite o nome da primeira bebida: `)),
+    nome2: (prompt(`Digite o nome da segunda bebida: `)),
+    valorTotal: (parseFloat(prompt(`Digite o valor total delas: `)))
+}; 
 
-grupo.consumoExtras.push(bebidas);
+grupo.consumoExtras.push(bebidas.nome1, bebidas.nome2);
 
-console.log(grupo);
+let subtotal = grupo.orcamentoTotal - bebidas.valorTotal
+let total = 0;
+
+if (subtotal < 0) {
+
+    grupo.openBarFechado = true;
+    grupo.multa = parseFloat(500);
+    subtotal = subtotal * -1;
+    console.log(`\n- O valor total ultrapassou o orçamento em R$${subtotal.toFixed(2)}`);
+    grupo.orcamentoTotal = 0;
+    let diferenca = subtotal * -1;
+    total = grupo.multa + diferenca;
+   
+} else {
+    grupo.orcamentoTotal = subtotal;
+}
+
+console.log(`\n--- Fechamento financeiro ---\n\nEmpresa: ${grupo.nome}\nOrçamento Total: R$${grupo.orcamentoTotal.toFixed(2)}
+Consumos Extras:\n- ${grupo.consumoExtras}\n\nTotal a pagar:R$${total.toFixed(2)}`);
