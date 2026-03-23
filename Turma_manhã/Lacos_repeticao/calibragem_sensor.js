1a467edad23ce8b543bf53355b2e7450d834a628
JigsawCesar <felipeportela_@hotmail.com>
2026-03-23 09:15:40 -0300

    update: adicionando funções

--- Turma_manhã/Lacos_repeticao/calibragem_sensor.js
+++ Turma_manhã/Lacos_repeticao/calibragem_sensor.js
@@ -2,10 +2,15 @@ const prompt = require(`prompt-sync`)();
 
 let temperaturaInicial = [];
 
-for (let i = 0; i < 5; i++){
+const calibragem_temperatura = (temperaturaInicial) => {
 
-    temperaturaInicial[i] = parseFloat(prompt(`Digite a temperatura: `));
-    temperaturaInicial[i] = temperaturaInicial[i] + 2;
-    console.log(`- Temperatura calibrada do sensor ${i + 1}: ${temperaturaInicial[i]}°C\n`);
+    for (let i = 0; i < 5; i++){
+
+        temperaturaInicial[i] = parseFloat(prompt(`Digite a temperatura: `));
+        temperaturaInicial[i] = temperaturaInicial[i] + 2;
+        console.log(`- Temperatura calibrada do sensor ${i + 1}: ${temperaturaInicial[i]}°C\n`);
+    };
+
 };
 
+calibragem_temperatura(temperaturaInicial);
--- Turma_manhã/Lacos_repeticao/cofre_digital.js
+++ Turma_manhã/Lacos_repeticao/cofre_digital.js
@@ -3,14 +3,19 @@ const prompt = require(`prompt-sync`)();
 let senha = 2404;
 let senhaDigitada = 0;
 
-do {
-    senhaDigitada = parseInt(prompt(`Digite a senha de acesso ao cofre: `));
+const validador_senha = (senhaDigitada) => {
+
+    do {
+        senhaDigitada = parseInt(prompt(`Digite a senha de acesso ao cofre: `));
     
-    if (senhaDigitada != senha) {
-        console.log(`- Acesso negado: Senha incorreta!\n\nDigite a senha novamente:`);    
-    }
+        if (senhaDigitada != senha) {
+            console.log(`- Acesso negado: Senha incorreta!\n\nDigite a senha novamente:`);    
+        }
     
 
-} while(senhaDigitada != senha);
+    } while(senhaDigitada != senha);
 
-console.log(`- Cofre liberado com sucesso!`);
+    console.log(`\n=============================\n- Cofre liberado com sucesso!\n=============================`);
+};
+
+validador_senha(senhaDigitada);
