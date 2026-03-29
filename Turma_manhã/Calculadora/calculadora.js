a036a3ba0e6fd6bab534e19c2fdb6672ac797e85
Felipe <felipeportela_@hotmail.com>
2026-03-29 20:09:14 -0300

    feat: Adiciona formatação e símbolos ao texto de entrada e saída da calculadora

--- Turma_manhã/Calculadora/calculadora.js
+++ Turma_manhã/Calculadora/calculadora.js
@@ -7,11 +7,11 @@ let resultado;
 let opcao;
 
 
-let nome = prompt(`Digite seu nome: `)
+let nome = prompt(`⦙ Digite seu nome: `)
 
 const saudadcao = () => {
     
-    console.log(`\n=================================\n= Olá ${nome}!\n= Seja bem-vindo a calculadora! =\n=================================\n`);
+    console.log(`\n=================================\n⦙ Olá ${nome}!\n⦙ Seja bem-vindo a calculadora! ⦙\n=================================\n`);
     setTimeout(function() {
         
         console.clear();
@@ -27,54 +27,58 @@ const menu = () => {
 =======================================
 =========== Menu de Opções ============
 =======================================
-= [1] - Soma                          =
-= [2] - Subtração                     =
-= [3] - Multiplicação                 =
-= [4] - Divisão                       =
-= [5] - Potenciação                   =
-= [6] - Raiz Quadrada                 =
-=                                     =
-= [0] - Sair                          =
+⦙ [1] - Soma                           ⦙
+⦙ [2] - Subtração                      ⦙
+⦙ [3] - Multiplicação                  ⦙
+⦙ [4] - Divisão                        ⦙
+⦙ [5] - Potenciação                    ⦙
+⦙ [6] - Raiz Quadrada                  ⦙
+⦙                                      ⦙
+⦙ [0] - Sair                           ⦙
 =======================================`);
     
-    opcao = parseInt(prompt(`= Digite a opção desejada: `));
+    opcao = parseInt(prompt(`⦙ Digite a opção desejada: `));
     console.clear();
 
     switch (opcao) {
         case 0:
             
-            console.log(`= Obrigado por usar a calculadora!\n= Até a próxima ${nome}!`);
+            console.log(`⦙ Obrigado por usar a calculadora!\n⦙ Até a próxima ${nome}!`);
     
         break;
         
         case 1:
     
-            number1 = Number(prompt(`= Digite o primeiro número: `));
-            number2 = Number(prompt(`= Digite o segundo número: `));
+            console.log();
+            number1 = Number(prompt(`⦙ Digite o primeiro número: `));
+            number2 = Number(prompt(`⦙ Digite o segundo número: `));
             adicao();
         
         break;
     
         case 2:
-    
-            number1 = Number(prompt(`= Digite o primeiro número: `));
-            number2 = Number(prompt(`= Digite o segundo número: `));
+
+            console.log();
+            number1 = Number(prompt(`⦙ Digite o primeiro número: `));
+            number2 = Number(prompt(`⦙ Digite o segundo número: `));
             subtracao();
         
         break;
     
         case 3:
         
-            number1 = Number(prompt(`= Digite o primeiro número: `));
-            number2 = Number(prompt(`= Digite o segundo número: `));
+            console.log();
+            number1 = Number(prompt(`⦙ Digite o primeiro número: `));
+            number2 = Number(prompt(`⦙ Digite o segundo número: `));
             multiplicacao();
         
         break;
     
         case 4:
     
-            number1 = Number(prompt(`= Digite o primeiro número: `));
-            number2 = Number(prompt(`= Digite o segundo número: `));
+            console.log();
+            number1 = Number(prompt(`⦙ Digite o primeiro número: `));
+            number2 = Number(prompt(`⦙ Digite o segundo número: `));
             divisao();
         
         break;
@@ -87,7 +91,6 @@ const menu = () => {
     
         case 6:
     
-            number1 = Number(prompt(`Digite o número que deseja saber a raiz quadrada: `));
             raiz_quadrada();
         
         break;
@@ -107,39 +110,42 @@ const menu = () => {
 const adicao = () => {
     
     resultado = number1 + number2;
-    return console.log(`= Resultado:\n= ${number1} + ${number2} = ${resultado}`);
+    return console.log(`⦙ Resultado:\n⦙ ${number1} + ${number2} = ${resultado}`);
+
 };
 
 const subtracao = () => {
 
     resultado = number1 - number2;
-    return console.log(`= Resultado:\n= ${number1} - ${number2} = ${resultado}`);
+    return console.log(`⦙ Resultado:\n⦙ ${number1} - ${number2} = ${resultado}`);
 };
 
 const multiplicacao = () => {
 
     resultado = number1 * number2;
-    return console.log(`= Resultado:\n= ${number1} * ${number2} = ${resultado}`);
+    return console.log(`⦙ Resultado:\n⦙ ${number1} * ${number2} = ${resultado}`);
 };
 
 const divisao = () => {
 
     resultado = number1 / number2;
-    return console.log(`= Resultado:\n= ${number1} / ${number2} = ${resultado}`);};
+    return console.log(`⦙ Resultado:\n⦙ ${number1} / ${number2} = ${resultado}`);};
 
 const potenciacao = () => {
 
-    number1 = Number(prompt(`= Digite o número que será a base da potenciação: `));
-    number2 = Number(prompt(`= Digite o número que será o expoente da potenciação: `));
+    console.log();
+    number1 = Number(prompt(`⦙ Digite o número que será a base da potenciação: `));
+    number2 = Number(prompt(`⦙ Digite o número que será o expoente da potenciação: `));
     resultado = Math.pow(number1, number2);
-    return console.log(`= Resultado:\n= ${number1} elevado a ${number2} = ${resultado}`);
+    return console.log(`⦙\n⦙ Resultado:\n⦙ ${number1} ^ ${number2} = ${resultado.toFixed(2)}`);
 };
 
 const raiz_quadrada = () => {
 
-    number1 = Number(prompt(`= Digite o número que deseja obter a raíz quadrada: `))
+    console.log();
+    number1 = Number(prompt(`⦙ Digite o número que deseja obter a raíz quadrada: `))
     resultado = Math.sqrt(number1);
-    return console.log(`= Resultado:\n ${number1}² = `)
+    return console.log(`⦙\n⦙ Resultado:\n⦙ √¯${number1} = ${resultado}`);
 
 };
 
