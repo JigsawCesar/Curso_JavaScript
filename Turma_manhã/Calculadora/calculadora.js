762236f96360b9caefad0165ee840537db19ab59
Felipe <felipeportela_@hotmail.com>
2026-04-01 22:41:22 -0300

    feat: Adiciona arquivo .gitignore para ignorar a pasta node_modules

--- Turma_manhã/Calculadora/calculadora.js
+++ Turma_manhã/Calculadora/calculadora.js
@@ -9,23 +9,22 @@ const amarelo = "\x1b[33m";
 const vermelho = "\x1b[31m";
 const magenta = "\x1b[35m";
 
-console.log();
 let number1 = 0;
 let number2 = 0;
 let resultado = 0;
 let opcao = 0;
 
-
+console.log();
 let nome = prompt(`${negrito}${ciano}⦙ Digite seu nome:${reset} `);
 console.clear();
 
 const saudadcao = () => {
     
     console.log(`${roxo}    
-╭──────────────────────────────
-│${reset}${negrito} Olá, ${verde}${nome}${reset}!${roxo}                              
-│${reset}${negrito} Seja bem-vindo à Calculadora!        ${roxo}
-╰──────────────────────────────${reset}`);
+╭───────────────────────────────
+│${reset} Olá, ${verde}${negrito}${nome}${reset}!${roxo}                              
+│${reset} Seja bem-vindo à Calculadora!        ${roxo}
+╰───────────────────────────────${reset}`);
     setTimeout(function() {
         
         console.clear();
@@ -36,99 +35,134 @@ const saudadcao = () => {
 }
 
 const adicao = () => {
-    
     console.log();
-    number1 = parseFloat(prompt(`${negrito}${ciano}⦙ Digite o primeiro número: ${reset}`));
-    number2 = parseFloat(prompt(`${negrito}${ciano}⦙ Digite o segundo número: ${reset}`));
-    
-    if (!(isNaN(number1) || isNaN(number2))) {
-        resultado = number1 + number2;
-        return console.log(`\n${negrito}${magenta}⦙ Resultado:\n⦙ ${number1} + ${number2} = ${resultado}${reset}`);
-    } else {
-        return console.log(`\n${negrito}${vermelho}⦙ Por favor, digite um número.${reset}`);
+
+    while (true) { 
+        number1 = parseFloat(prompt(`${negrito}${ciano}⦙ Digite o número: ${reset}`));
+        number2 = parseFloat(prompt(`${negrito}${ciano}⦙ Digite o número a ser somado: ${reset}`));
+        console.clear();
+        
+        if (!(isNaN(number1) || isNaN(number2))) {
+            resultado = number1 + number2;
+            console.log(`\n${negrito}${magenta}⦙ Resultado:\n⦙ ${number1} + ${number2} = ${resultado}${reset}\n`);
+            prompt(`${negrito}${ciano}⦙ Pressione Enter para voltar ao menu ...${reset}`);
+            break;
+        } else {
+            console.log(`\n${negrito}${vermelho}⦙ ⚠️ Entrada inválida! Por favor, digite um número.${reset}\n`);
+        }; 
     };
 };
 
 const subtracao = () => {
-
     console.log();
-    number1 = Number(prompt(`${negrito}${ciano}⦙ Digite o primeiro número: ${reset}`));
-    number2 = Number(prompt(`${negrito}${ciano}⦙ Digite o segundo número: ${reset}`));
 
-    if (typeof number1 === `number` && typeof number2 === `number`){
-        resultado = number1 - number2;
-        return console.log(`\n${negrito}${magenta}⦙ Resultado:\n⦙ ${number1} - ${number2} = ${resultado}${reset}`);
-    } else {
-        return console.log(`\n${negrito}${vermelho}⦙ Por favor, digite um número.${reset}`);
+    while (true) {
+        number1 = Number(prompt(`${negrito}${ciano}⦙ Digite o número: ${reset}`));
+        number2 = Number(prompt(`${negrito}${ciano}⦙ Digite o número a ser subitraído: ${reset}`));
+        console.clear();
+
+        if (!(isNaN(number1) || isNaN(number2))) {
+            resultado = number1 - number2;
+            console.log(`\n${negrito}${magenta}⦙ Resultado:\n⦙ ${number1} - ${number2} = ${resultado}${reset}\n`);
+            prompt(`${negrito}${ciano}⦙ Pressione Enter para voltar ao menu ...${reset}`);
+            break;
+        } else {
+                console.log(`\n${negrito}${vermelho}⦙ ⚠️ Entrada inválida! Por favor, digite um número.${reset}\n`);
+        };
     };
 };
 
 const multiplicacao = () => {
-
     console.log();
-    number1 = Number(prompt(`${negrito}${ciano}⦙ Digite o primeiro número: ${reset}`));
-    number2 = Number(prompt(`${negrito}${ciano}⦙ Digite o segundo número: ${reset}`));
 
-    if (typeof number1 == `number` && typeof number2 == `number`){
-    resultado = number1 * number2;
-    return console.log(`\n${negrito}${magenta}⦙ Resultado:\n⦙ ${number1} * ${number2} = ${resultado}${reset}`);
-    } else {
-        return console.log(`\n${negrito}${vermelho}⦙ Por favor, digite um número.${reset}`);
+    while (true) {
+        number1 = Number(prompt(`${negrito}${ciano}⦙ Digite o número: ${reset}`));
+        number2 = Number(prompt(`${negrito}${ciano}⦙ Digite o número multiplicador: ${reset}`));
+        console.clear();
+
+        if (!(isNaN(number1) || isNaN(number2))) {
+            resultado = number1 * number2;
+            console.log(`\n${negrito}${magenta}⦙ Resultado:\n⦙ ${number1} * ${number2} = ${resultado}${reset}\n`);
+            prompt(`${negrito}${ciano}⦙ Pressione Enter para voltar ao menu ...${reset}`);
+            break;
+        } else {
+            console.log(`\n${negrito}${vermelho}⦙ ⚠️ Entrada inválida! Por favor, digite um número.${reset}\n`);
+        };
     };
 };
 
 const divisao = () => {
-
     console.log();
-    number1 = Number(prompt(`${negrito}${ciano}⦙ Digite o primeiro número: ${reset}`));
-    number2 = Number(prompt(`${negrito}${ciano}⦙ Digite o segundo número: ${reset}`));
 
-    if (typeof number1 == `number` && typeof number2 == `number`){
-        resultado = number1 / number2;
-        return console.log(`\n${negrito}${magenta}⦙ Resultado:\n⦙ ${number1} / ${number2} = ${resultado}${reset}`);
-    } else {
-        return console.log(`\n${negrito}${vermelho}⦙ Por favor, digite um número.${reset}`);
-    };   
+    while (true) {
+        number1 = Number(prompt(`${negrito}${ciano}⦙ Digite o número: ${reset}`));
+        number2 = Number(prompt(`${negrito}${ciano}⦙ Digite o número divisor: ${reset}`));
+        console.clear();
+
+        if (!(isNaN(number1) || isNaN(number2))) {
+            resultado = number1 / number2;
+            console.log(`\n${negrito}${magenta}⦙ Resultado:\n⦙ ${number1} / ${number2} = ${resultado.toFixed(2)}${reset}\n`);
+            prompt(`${negrito}${ciano}⦙ Pressione Enter para voltar ao menu ...${reset}`);
+            break;
+        } else {
+            console.log(`\n${negrito}${vermelho}⦙ ⚠️ Entrada inválida! Por favor, digite um número.${reset}\n`);
+        };   
+    };
 };
 
 const potenciacao = () => {
-
     console.log();
-    number1 = Number(prompt(`${negrito}${ciano}⦙ Digite o número que será a base da potenciação: ${reset}`));
-    number2 = Number(prompt(`${negrito}${ciano}⦙ Digite o número que será o expoente da potenciação: ${reset}`));
+    
+    while (true) {
+        number1 = Number(prompt(`${negrito}${ciano}⦙ Digite o número que será a base da potenciação: ${reset}`));
+        number2 = Number(prompt(`${negrito}${ciano}⦙ Digite o número que será o expoente da potenciação: ${reset}`));
+        console.clear();
 
-    if (typeof number1 == `number` && typeof number2 == `number`){
-        resultado = Math.pow(number1, number2);
-        return console.log(`\n${negrito}${magenta}⦙ Resultado:\n⦙ ${number1} ^ ${number2} = ${resultado.toFixed(2)}${reset}`);
-    } else {
-        return console.log(`\n${negrito}${vermelho}⦙ Por favor, digite um número.${reset}`);
+        if (!(isNaN(number1) || isNaN(number2))) {
+            resultado = Math.pow(number1, number2);
+            console.log(`\n${negrito}${magenta}⦙ Resultado:\n⦙ ${number1} ^ ${number2} = ${resultado}${reset}\n`);
+            prompt(`${negrito}${ciano}⦙ Pressione Enter para voltar ao menu ...${reset}`);
+            break;
+        } else {
+            console.log(`\n${negrito}${vermelho}⦙ ⚠️Entrada inválida! Por favor, digite um número.${reset}\n`);
+        };
     };   
 };
 
 const raiz_quadrada = () => {
-
     console.log();
-    number1 = Number(prompt(`${negrito}${ciano}⦙ Digite o número que deseja obter a raíz quadrada: ${reset}`))
     
-    if (typeof number1 == `number`){
-        resultado = Math.sqrt(number1);
-        return console.log(`\n${negrito}${magenta}⦙ Resultado:\n⦙ √¯${number1} = ${resultado}${reset}`);
-    } else {
-        return console.log(`\n${negrito}${vermelho}⦙ Por favor, digite um número.${reset}`);
+    while (true) {
+        number1 = Number(prompt(`${negrito}${ciano}⦙ Digite o número que deseja obter a raíz quadrada: ${reset}`));
+        console.clear();
+        
+        if (!(isNaN(number1))) {
+            resultado = Math.sqrt(number1);
+            console.log(`\n${negrito}${magenta}⦙ Resultado:\n⦙ √¯ ${number1} = ${resultado}${reset}\n`);
+            prompt(`${negrito}${ciano}⦙ Pressione Enter para voltar ao menu ...${reset}`);
+            break;
+        } else {
+            console.log(`\n${negrito}${vermelho}⦙ ⚠️ Entrada inválida! Por favor, digite um número.${reset}\n`);
+        };
     };   
 };
 
 const porcentagem = () => {
-    
     console.log();
-    number1 = Number(prompt(`${negrito}${ciano}⦙ Digite o número que deseja obter a porcentagem: ${reset}`));
-    number2 = Number(prompt(`${negrito}${ciano}⦙ Digite a porcentagem que deseja obter: ${reset}`));
     
-    if (typeof number1 == `number`){
-        resultado = (number1 * number2) / 100;
-        return console.log(`\n${negrito}${magenta}⦙ Resultado:\n⦙ ${number2}% de ${number1} = ${resultado}${reset}`);
-    } else {
-        return console.log(`\n${negrito}${vermelho}⦙ Por favor, digite um número.${reset}`);
+    while (true) {
+        number1 = Number(prompt(`${negrito}${ciano}⦙ Digite o número que deseja obter a porcentagem: ${reset}`));
+        number2 = Number(prompt(`${negrito}${ciano}⦙ Digite a porcentagem que deseja obter: ${reset}`));
+        console.clear();
+        
+        if (!(isNaN(number1) || isNaN(number2))) {
+            resultado = (number1 * number2) / 100;
+            console.log(`\n${negrito}${magenta}⦙ Resultado:\n⦙ ${number2}% de ${number1} = ${resultado}${reset}\n`);
+            prompt(`${negrito}${ciano}⦙ Pressione Enter para voltar ao menu ...${reset}`);
+            break;
+        } else {
+            console.log(`\n${negrito}${vermelho}⦙ ⚠️ Entrada inválida! Por favor, digite um número.${reset}\n`);
+        };
     };   
 };
 
