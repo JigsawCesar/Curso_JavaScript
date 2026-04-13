d4e2b6712b7e351fa6f3b5795fef12e5c0a482ad
Felipe <felipeportela_@hotmail.com>
2026-04-13 19:59:15 -0300

    feat: renomeia função de remover para remover_numero e melhora a formatação das mensagens

--- Turma_manhã/Calculadora_Estatistica/remover.js
+++ Turma_manhã/Calculadora_Estatistica/remover.js
@@ -1,5 +1,13 @@
-const remover = (lista) =>{
-    
+import { negrito, magenta, ciano, verde, reset } from "../Calculadora/cores_terminal.js";
+import PromptSync from "prompt-sync";
+
+const prompt = PromptSync();
+
+const remover_numero = (lista) =>{
+    lista.pop();
+    console.log(`${negrito}${magenta}⦙ Número removido com sucesso!${reset}`);
+    console.log(`${negrito}${magenta}⦙ Lista atual:${reset} ${lista.join(", ")}\n`);
+    prompt(`${negrito}${ciano}⦙ Pressione ${verde}Enter${reset} ${ciano}para voltar ao menu ...${reset}`);
 };
 
-export default remover;
+export default remover_numero;
