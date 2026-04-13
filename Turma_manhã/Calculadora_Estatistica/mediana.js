24a27f3c786ebb2401bdae31b07899d990f0c82e
Felipe <felipeportela_@hotmail.com>
2026-04-13 19:58:54 -0300

    feat: renomeia função de mediana para calcular_mediana e melhora a mensagem de erro para lista vazia

--- Turma_manhã/Calculadora_Estatistica/mediana.js
+++ Turma_manhã/Calculadora_Estatistica/mediana.js
@@ -1,5 +1,21 @@
-const mediana = (lista) =>{
+import { negrito, vermelho, reset } from "../Calculadora/cores_terminal.js";
+
+const calcular_mediana = (lista) =>{
     
+    let mediana = 0;
+    
+    if (lista.length === 0){
+        console.log(`\n${negrito}${vermelho}⦙ A lista está vazia!${reset}\n`);
+        return 0;
+    };
+    
+    if(lista.length % 2 == 0){
+        mediana = (lista[lista.length / 2] + lista[(lista.length / 2) - 1]) / 2
+    } else {
+        mediana = lista[Math.floor(lista.length / 2)]
+    };
+
+    return mediana;
 };
 
-export default mediana;
+export default calcular_mediana;
