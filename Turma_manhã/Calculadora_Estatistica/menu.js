cd5cf5fd912ac4a3e9f6e21acfd8e0add102a986
Felipe <felipeportela_@hotmail.com>
2026-04-13 19:59:01 -0300

    feat: atualiza opções do menu para listar números e reorganiza a ordem das funções

--- Turma_manhã/Calculadora_Estatistica/menu.js
+++ Turma_manhã/Calculadora_Estatistica/menu.js
@@ -9,8 +9,9 @@ ${roxo}====================================================
 ${roxo}⦙                                                   ⦙
 ${roxo}⦙ ${negrito}${amarelo}[1]${reset} - ${amarelo}Adicionar números a uma lista${reset}               ${roxo}⦙
 ${roxo}⦙ ${negrito}${amarelo}[2]${reset} - ${amarelo}Remover o último número adicionado a lista${reset}  ${roxo}⦙
-${roxo}⦙ ${negrito}${amarelo}[3]${reset} - ${amarelo}Calcular a Média da lista${reset}                   ${roxo}⦙
-${roxo}⦙ ${negrito}${amarelo}[4]${reset} - ${amarelo}Calcular a Mediana da lista${reset}                 ${roxo}⦙
+${roxo}⦙ ${negrito}${amarelo}[3]${reset} - ${amarelo}Listar os números${reset}                           ${roxo}⦙
+${roxo}⦙ ${negrito}${amarelo}[4]${reset} - ${amarelo}Calcular a Média da lista${reset}                   ${roxo}⦙
+${roxo}⦙ ${negrito}${amarelo}[5]${reset} - ${amarelo}Calcular a Mediana da lista${reset}                 ${roxo}⦙
 ${roxo}⦙                                                   ⦙
 ${roxo}⦙ ${vermelho}[0]${reset} - ${vermelho}Sair${reset}                                        ${roxo}⦙
 ${roxo}====================================================${reset}`);
