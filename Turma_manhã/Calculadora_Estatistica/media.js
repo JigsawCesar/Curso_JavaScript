48f8fe9d4c14f671e4901bae013377372efefef2
Felipe <felipeportela_@hotmail.com>
2026-04-13 19:58:28 -0300

    feat: renomeia função de média e adiciona mensagem de erro para lista vazia

--- Turma_manhã/Calculadora_Estatistica/media.js
+++ Turma_manhã/Calculadora_Estatistica/media.js
@@ -1,7 +1,13 @@
-const media = (lista) =>{
-    if (lista.length === 0) return 0;
-    let soma = lista.reduce((acumulador, valor_atual) => acumulador + valor_atual, 0);
-    return soma / lista.length;
+import { negrito, vermelho, reset } from "../Calculadora/cores_terminal.js";
+
+const calcular_media = (lista) =>{
+    if (lista.length === 0) {
+        console.log(`\n${negrito}${vermelho}⦙ A lista está vazia!${reset}\n`);
+        return 0;
+    } else {
+        let soma = lista.reduce((acumulador, valor_atual) => acumulador + valor_atual, 0);
+        return soma / lista.length;
+    }
 };
 
-export default media;
+export default calcular_media;
