f84f96ef7625386cfc4915a48fc97d05a170cf41
Felipe <felipeportela_@hotmail.com>
2026-04-13 19:58:08 -0300

    feat: renomeia função para adicionar número e melhora a formatação das mensagens

--- Turma_manhã/Calculadora_Estatistica/adicionar.js
+++ Turma_manhã/Calculadora_Estatistica/adicionar.js
@@ -1,5 +1,13 @@
-const adicionar = (lista) =>{
-    
+import PromptSync from "prompt-sync";
+import { negrito, ciano, magenta, verde, reset } from "../Calculadora/cores_terminal.js";
+
+const prompt = PromptSync();
+
+const adicionar_numero = (lista, numero) =>{
+    lista.push(numero);
+    console.log(`${negrito}${magenta}⦙ Número adicionado com sucesso!${reset}`);
+    console.log(`${negrito}${magenta}⦙ Lista atual:${reset} ${lista.join(", ")}\n`);
+    prompt(`${negrito}${ciano}⦙ Pressione ${verde}Enter${reset} ${ciano}para voltar ao menu ...${reset}`);
 };
 
-export default adicionar;
+export default adicionar_numero;
