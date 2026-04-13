6cfe295c3d1a69939403541f5c9611a80f5cc342
Felipe <felipeportela_@hotmail.com>
2026-04-13 19:57:56 -0300

    feat: remove espaços em branco desnecessários na função de saudação

--- Turma_manhã/Calculadora/saudacao.js
+++ Turma_manhã/Calculadora/saudacao.js
@@ -4,19 +4,13 @@ import { negrito, reset, roxo, verde } from "./cores_terminal.js";
 const prompt = PromptSync();
 
 const saudacao = (nome) => {
-    
-
     console.log(`${roxo}    
 ╭──────────────────────────────────╮
 │${reset} Olá, ${verde}${negrito}${nome}${reset}!${roxo}                              
 │${reset} Seja bem-vindo à Calculadora!    ${roxo}│
 ╰──────────────────────────────────╯${reset}\n`);
-
     prompt (`Pressione ${verde}${negrito}Enter${reset} para continuar...`);
     console.clear();
-
-
-    
 };
 
 export default saudacao;
