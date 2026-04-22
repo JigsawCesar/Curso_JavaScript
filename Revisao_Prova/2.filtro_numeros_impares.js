05d6e1084c44423afdabbf47bccd19b3935db565
JigsawCesar <felipeportela_@hotmail.com>
2026-04-22 17:23:00 -0300

    fix: correção de pontuação no código

--- Revisao_Prova/2.filtro_numeros_impares.js
+++ Revisao_Prova/2.filtro_numeros_impares.js
@@ -1,12 +1,12 @@
-import { ciano, verde, reset } from "../Cores/cores_terminal.js"
+import { ciano, verde, reset } from "../Cores/cores_terminal.js";
 
 let lista = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ];
-let contador = 0
+let contador = 0;
 
 for (let i = 0; i < lista.length; i++) {
     
     let valor = parseInt(lista[i]);
-    valor = valor % 2
+    valor = valor % 2;
     
     if (valor !== 0) {
         contador = contador + 1;
