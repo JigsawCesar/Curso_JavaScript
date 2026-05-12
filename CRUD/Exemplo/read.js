f996973dd38b8d16b3911589f2cc5d12c5fabc26
JigsawCesar <felipeportela_@hotmail.com>
2026-05-12 12:04:03 -0300

    refactor: simplifica a função listar_contato removendo logs e retornando mensagem adequada

--- CRUD/Exemplo/read.js
+++ CRUD/Exemplo/read.js
@@ -1,18 +1,10 @@
-import { ciano, vermelho, negrito, reset } from "../../Calculadora/cores_terminal.js";
-
 const listar_contato = (contatos) => {
     
     if (contatos.length === 0) {
-        console.log(`${negrito}${vermelho}\n⦙ Nenhum contato cadastrado.${reset}`);
-        return
+        return "⦙ Nenhum contato cadastrado.";
     };
-
-    contatos.forEach(contato => {
-        console.log(`\n${negrito}${ciano}\n⦙ ID:${reset} ${contato.id}`);
-        console.log(`${negrito}${ciano}\n⦙ Nome:${reset} ${contato.nome}`);
-        console.log(`${negrito}${ciano}\n⦙ Email:${reset} ${contato.email}`);
-        console.log(`${negrito}${ciano}\n⦙ Telefones:${reset} ${contato.telefones.join(" | ")}`);
-    });
+    
+    return contatos
 };
 
 export default listar_contato;
