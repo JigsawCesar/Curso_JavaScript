69239658b37c15c2c2196a897413cf8e5f621fd5
JigsawCesar <felipeportela_@hotmail.com>
2026-05-13 11:35:32 -0300

    feat: adiciona a função listar_contato para exibir contatos ou mensagem quando não há contatos

---
+++ API/read.js
@@ -0,0 +1,10 @@
+const listar_contato = (contatos) => {
+    
+    if (contatos.length === 0) {
+        return "⦙ Nenhum contato cadastrado.";
+    };
+    
+    return contatos
+};
+
+export default listar_contato;
