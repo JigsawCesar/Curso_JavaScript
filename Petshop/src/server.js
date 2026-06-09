b1e6645617964f152dc46f43271af229955d7fc3
JigsawCesar <felipeportela_@hotmail.com>
2026-06-09 10:52:03 -0300

    feat: adiciona rotas para busca, atualização e deleção de agendamentos

--- Petshop/src/server.js
+++ Petshop/src/server.js
@@ -70,7 +70,7 @@ router.post(("/agendamentos"), async (req, res) =>{
                 default:
                     console.log("Serviço inválido!");
                     break;
-            }
+            };
         
         }
 
@@ -91,6 +91,58 @@ router.post(("/agendamentos"), async (req, res) =>{
         res.status(400).json({ message: `Erro ao criar o agendamento! ${ erro.message }`})
     };
 });
+
+router.get(("/agendamentos"), async (req,res) => {
+    try {
+        const todos_agendamentos = await Agendamento.find();
+        res.status(200).json({ message: `Todos os elementos foram listados com sucesso!`, agendamentos: todos_agendamentos })
+    } catch (erro) {
+        res.status(400).json({ message: `Erro ao listar os agendamentos! ${ erro.message }` })
+    };
+});
+
+router.get(("/agendamentos/busca"), async (req,res) =>{
+    try {
+        const nome = req.query.nome;
+        const agendamentos = await Agendamento.find({ nome_pet: { $regex: nome, $options: "i"} }); //regex busca por partes do texto e partes da string e option ignora maiúsculas e minúsculas 
+        res.status(200).json({ message: "Busca efetuada com sucesso!", agendamentos: agendamentos })
+    } catch (erro) {
+        res.status(400).json({ message: `Erro: ${ erro.message }` })
+    }
+});
+
+router.patch(("/agendamentos/:id"), async (req,res) => {
+    try {
+        const id = req.params.id;
+        const { status } = req.body;
+        const agendamento_atualizado = await Agendamento.findByIdAndUpdate(id, { status: status}, {runValidators: true, new: true});
+
+        if (!agendamento_atualizado) {
+            return res.status(404).json({ message: "Agendamento não encontrado!" });
+        } else {
+            res.status(200).json({ message: `Agendamento atualizado com sucesso!`, agendamento_atualizado: agendamento_atualizado });
+        };
+
+    } catch (erro) {
+        res.status(500).json({ message: `Erro de servidor: ${ erro.message }` });
+    }
+});
+
+router.delete(("/agendamentos/:id"), async (req,res) =>{
+    try {
+        const id = req.params.id;
+        const agendamento_deletado = Agendamento.findByIdAndDelete(id);
+
+        if(!agendamento_deletado) {
+            return res.status(404).json({ message: "Agendamento não encontrado" });
+        } else {
+            res.status(200).json({ message: "Agendamento deletado com sucesso!", agendamento_deletado: agendamento_deletado })
+        };
+
+    } catch (erro) {
+        res.status(500).json({ message: `Erro de servidor: ${ erro.message }` });
+    };
+})
 
 router.listen((PORT), () => {
     console.log(`Conectado com a porta ${PORT} com sucesso!`);
